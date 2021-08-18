2a4925d87890c343c6b82267cc8172b2ebde6bba
Luke Simon <lukesimon0@gmail.com>
2021-08-18 13:22:17 -0700

    pushing my code

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# README_Generator
+
+<h1 align ="center">Generate README.md </h1>
+
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)<br />
+## Description
+ To Generate a quality README file using Node.js
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+ Inquirer installation is required for this assignment
+## Usage
+ UCI Coding Bootcamp
+## License
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+<br />
+This application is covered by the MIT license. 
+## Contributing
+ Luke Simon
+## Tests
+ No test
+## Questions
+ Email<br />
+<br />
+:octocat: Find me on GitHub: [LukeSimon32](https://github.com/LukeSimon32)<br />
+<br />
+ Email me with any questions: lukesimon0@gmail.com<br /><br />
---
+++ index.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateMarkdown.js")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Description of project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Required Installations: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Contributors?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Included tests: "
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Email: "
+        }
+    ]);
+} 
+
+// Async function using util.promisify 
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();
